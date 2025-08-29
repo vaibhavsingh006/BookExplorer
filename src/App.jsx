7b072a5f63d9f92e5193540a528d4279d0af3ab5
vaibhavsingh006 <vaibhavsinghchandel20@gmail.com>
2025-08-29 19:22:58 +0530

    path-fix-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Toaster } from "./components/ui/Toaster";
 import { Toaster as Sonner } from "./components/ui/Sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
+import { TooltipProvider } from "./components/ui/Tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
